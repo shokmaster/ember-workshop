af34a4ab1608bf5dcb3a9b9e637817955ea151e4
Juan Antonio Gomez Benito <juanantonio.gomez.benito@bbva.com>
2016-10-13 10:36:51 +0200

    [fix] Missing semicolon

--- app/controllers/restaurants/detail.js
+++ app/controllers/restaurants/detail.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
 	img: Ember.computed('model.id', function(){
-		return `https://github.com/IgorMinar/foodme/blob/master/app/img/restaurants/${this.get('model.id')}.jpg?raw=true`
+		return `https://github.com/IgorMinar/foodme/blob/master/app/img/restaurants/${this.get('model.id')}.jpg?raw=true`;
 	})
 });
