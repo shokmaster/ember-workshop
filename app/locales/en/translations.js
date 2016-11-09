fbac52dbfb5de2d67ae18ee57ad92321f5e8fac3
Juan Antonio Gomez Benito <juanantonio.gomez.benito@bbva.com>
2016-11-09 14:37:44 +0100

    [i18n] Add languages

---
+++ app/locales/en/translations.js
@@ -0,0 +1,21 @@
+export default {
+    // "some.translation.key": "Text for some.translation.key",
+    //
+    // "a": {
+    //   "nested": {
+    //     "key": "Text for a.nested.key"
+    //   }
+    // },
+    //
+    // "key.with.interpolation": "Text with {{anInterpolation}}"
+
+    sections: {
+    	aboutUs: 'About us',
+    	restaurants: 'Restaurants'
+    },
+
+    messages: {
+    	bestRestaurants: 'There are the best restaurants'
+    }
+
+};
---
+++ app/locales/es/translations.js
@@ -0,0 +1,21 @@
+export default {
+    // "some.translation.key": "Text for some.translation.key",
+    //
+    // "a": {
+    //   "nested": {
+    //     "key": "Text for a.nested.key"
+    //   }
+    // },
+    //
+    // "key.with.interpolation": "Text with {{anInterpolation}}"
+
+    sections: {
+    	aboutUs: 'Acerca de',
+    	restaurants: 'Restaurantes'
+    },
+
+    messages: {
+    	bestRestaurants: 'Estos son los mejores restaurantes'
+    }
+
+};
